3f50be83f74509fd53fbccc9ff2bea64b4d791ae
Yadhuvanshi <yaduvanshi742@proton.me>
2026-08-07 15:57:50 +0530

    Add downloadFile function to download files

---
+++ src/utils/download.js
@@ -0,0 +1,11 @@
+export function downloadFile(filename, content, type = 'application/json') {
+  const blob = new Blob([content], { type });
+  const url = URL.createObjectURL(blob);
+  const anchor = document.createElement('a');
+  anchor.href = url;
+  anchor.download = filename;
+  document.body.append(anchor);
+  anchor.click();
+  anchor.remove();
+  URL.revokeObjectURL(url);
+}
