4fb2b7c44ebd586f5372a34efdcb1b1985c01e50
Yadhuvanshi <yaduvanshi742@proton.me>
2026-08-14 12:56:40 +0530

    Add service worker for caching and offline support

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'datavista-js-cache';
+const APP_FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './public/icons/icon.svg',
+  './src/styles/main.css',
+  './src/app/main.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => caches.match('./index.html')))
+  );
+});
